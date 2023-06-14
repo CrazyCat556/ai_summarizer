d60f160d360ffbeba8608d365c6e6aa2887d2a15
CrazyCat556 <135308472+CrazyCat556@users.noreply.github.com>
2023-06-14 16:30:29 +0800

    Copy to Clipboard

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,6 +10,8 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+
   const [ getSummary, { error, isFetching}] = useLazyGetSummaryQuery();
 
   useEffect(() => {
@@ -31,6 +33,12 @@ const Demo = () => {
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
   }
 
   return (
@@ -57,8 +65,8 @@ const Demo = () => {
         <div className='flex flex-col gap-1 max-h-60overflow-y-auto'>
           {allArticles.map((item, index) => (
             <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
-              <div className='copy_btn'>
-                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img src={copied === item.url ? tick : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
               </div>
               <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
             </div>
